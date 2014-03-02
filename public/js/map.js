7e8dc6eaf3a290236c742c2ddea8fc27523de076
Jonathan Hudson <jonathanhudson82@gmail.com>
2014-03-02 18:47:55 +0000

    Add select element to view by category

--- public/js/map.js
+++ public/js/map.js
@@ -1,31 +1,28 @@
 $(document).ready(function() {
 
-    var myMap = {
+    function initialize() {              
         
-        initialize: function() {              
-            
-            var mapOptions = {
-                center: new google.maps.LatLng(51.46881, -0.12544),
-                zoom: 15
-            };
-            
-            var map = new google.maps.Map(document.getElementById("map-canvas"), mapOptions);
-            
-            $.get('/get-crimes', function(data) {
-                for (var i = 0; i < data.length; i++) {
-                    var marker = new google.maps.Marker({
-                        position: new google.maps.LatLng(data[i].location.latitude, data[i].location.longitude),
-                        map: map,
-                        title: data[i].category,
-                    });
-                }
-            }); 
-        }
-    }
-
-    google.maps.event.addDomListener(window, 'load', myMap.initialize());
+        var mapOptions = {
+            center: new google.maps.LatLng(51.46881, -0.12544),
+            zoom: 15
+        };
+        
+        var map = new google.maps.Map(document.getElementById("map-canvas"), mapOptions);
+        
+        $.get('/get-crimes', function(data) {
+            for (var i = 0; i < data.length; i++) {            
+                var marker = new google.maps.Marker({
+                    position: new google.maps.LatLng(data[i].location.latitude, data[i].location.longitude),
+                    map: map,
+                    title: data[i].category,
+                });
+            }
+        }); 
+    }   
     
+    google.maps.event.addDomListener(window, 'load', initialize);
 });
+
     
 
 
--- src/templates/home.php
+++ src/templates/home.php
@@ -1,12 +1,22 @@
 <!DOCTYPE html>
 <html>
     <head>
-        <link href="/css/style.css" type="text/css" />
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+        <link rel="stylesheet" href="/css/style.css" type="text/css">
     </head>
     <body>
+        <form action="/save-category" method="POST">
+            <select name="category">
+                <?php foreach($cats as $cat): ?>                    
+                    <?php $selected = ($selectedCatUrl == $cat->url) ? 'selected' : ''; ?>
+                    <option value="<?php echo $cat->url; ?>" <?php echo $selected; ?>><?php echo $cat->name; ?></option>
+                <?php endforeach; ?>
+            </select>
+            <button>Submit</button>
+        </form>
         <div id="map-canvas"></div>
         <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBoJfc5hCodqzH1IXFrFiAW313_B8CnCTA&sensor=true"></script>
-        <script src="http://code.jquery.com/jquery-1.11.0.min.js"></script>
-        <script src="/js/map.js"></script>
+        <script type="text/javascript" src="http://code.jquery.com/jquery-1.11.0.min.js"></script>
+        <script type="text/javascript" src="/js/map.js"></script>
     </body>
 </html>
